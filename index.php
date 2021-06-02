60c27802b0cbd54966de6aae7bff6b931b8f1b3f
Karimu Mohammed <Karimu.mohammed@gmail.com>
2021-06-01 23:53:24 -0400

    Subject: PHP Redirect to allow for Heroku app to go directly to main page

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once(“home.html”); ?>
+<!-- Added this file based on  https://medium.com/@agavitalis/how-to-deploy-a-simple-static-html-website-on-heroku-492697238e48
+
+Further code edits based on https://code.tutsplus.com/tutorials/how-to-redirect-with-php--cms-34680-->
+
+
+<?php 
+header("Location: https://environmental-justice-mockup.herokuapp.com/home.html");
+exit();
+?>
